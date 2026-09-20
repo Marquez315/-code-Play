8976f904abcd81846383ff5e52455c36225a4c97
Marquez315 <3758cam@gmail.com>
2026-09-20 18:29:29 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+// Service Worker para Ñandutí Play
+const CACHE_NAME = 'nanduti-play-v1';
+const urlsToCache = [
+  './',
+  './Ñandutí-Play-con-niveles-completos.html',
+  './manifest.json'
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activación del Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptación de solicitudes
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
